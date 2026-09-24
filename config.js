c00bb761a644499c6868e3a7a59dd6089a2ae9cc
Mimi3100 <142289355+Mimi3100@users.noreply.github.com>
2026-09-23 21:32:28 -0400

    Refactor APP_CONFIG in config.js

--- config.js
+++ config.js
@@ -6,11 +6,10 @@ window.APP_CONFIG = {
   appName: "Nuestra Aventura · Family Hub",
 
   SUPABASE_URL: "https://zolwiqjlboiqlwmjcnyc.supabase.co",
-  SUPABASE_KEY: "sb_publishable_yAqisND70WXvo2o-Sk5Lvg_-8oIDLKq",
 
-   //  GitHub Pages
+  // Compatible con las dos versiones del código
+  SUPABASE_KEY: "sb_publishable_yAqisND70WXvo2o-Sk5Lvg_-8oIDLKq",
+  SUPABASE_ANON_KEY: "sb_publishable_yAqisND70WXvo2o-Sk5Lvg_-8oIDLKq",
 
   SITE_URL: "https://mimi3100.github.io/Family-Vacay-Planer/"
 };
-
- 
